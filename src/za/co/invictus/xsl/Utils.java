40381eb6eda4ccd51bc0e4ef55b681ccdea3bdaa
mcdevilliers <martindevilliers@gmail.com>
2016-07-21 11:17:38 +0200

    XSLT Utils - Initial version

---
+++ src/za/co/invictus/xsl/Utils.java
@@ -0,0 +1,43 @@
+package za.co.invictus.xsl;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class Utils {
+	public static boolean compare(String i1, String i2) {
+		System.out.println("item 1=" + i1 + ", item 2=" + i2);
+		return false;
+	}
+	
+	public static boolean compareQueues(NodeList q1, NodeList q2) {
+		
+		if ((q1.getLength() == q2.getLength()) == false) {
+			return false;
+		}
+		
+		for (int i=0; i<q1.getLength(); i++) {
+			//System.out.println("comparing " + q1.item(i).getTextContent() + " to " + q2.item(i).getTextContent());
+			
+			String v1 = q1.item(i).getTextContent();
+			
+			Node i2 = q2.item(i);
+			String v2 = null;
+			
+			if (i2 != null) {
+				v2 = i2.getTextContent();
+			}
+			
+			if (v2 == null) {
+				return false;
+			}
+			
+			if (v1.matches(v2) == false) {
+				return false;
+			}
+		}
+		
+		return true;
+		
+	}
+	
+}
